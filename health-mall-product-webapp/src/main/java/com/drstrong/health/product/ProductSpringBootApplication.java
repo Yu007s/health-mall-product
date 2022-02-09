dc7e7055466e1326ef912ec67e3a127be3d6a4d7
liuqiuyi <liuqiuyi@cnstrong.cn>
2022-02-09 16:28:58 +0800

    重新部署test2

--- health-mall-product-webapp/src/main/java/com/drstrong/health/product/ProductSpringBootApplication.java
+++ health-mall-product-webapp/src/main/java/com/drstrong/health/product/ProductSpringBootApplication.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.drstrong.health.product", "com.drstrong.health.redis"})
 public class ProductSpringBootApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProductSpringBootApplication.class, args);
 	}
 }
