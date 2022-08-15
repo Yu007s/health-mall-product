5f44989fafca4e3569d2e2e2599f705e7cafb429
lishixuan <lishixuan@cnstrong.cn>
2022-08-15 14:02:27 +0800

    增加查询老的桐庐地区配送费远程接口

---
+++ health-mall-product-model/src/main/java/com/drstrong/health/product/model/entity/store/OldAreaFreight.java
@@ -0,0 +1,76 @@
+package com.drstrong.health.product.model.entity.store;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 老的桐庐邮费
+ * FIXME 仅作为数据修复使用,不要用于其它用途
+ *
+ * @author lsx
+ * @date 2022/8/5 14:48
+ */
+@Data
+@TableName("freight_pharmacy_area")
+public class OldAreaFreight implements Serializable {
+	private static final long serialVersionUID = 6721808163120768686L;
+
+	@TableId(type = IdType.AUTO)
+	private Long id;
+
+	/**
+	 * 医院自有药房ID
+	 */
+	private Long warehouseId;
+
+	/**
+	 * 第三方药店ID
+	 */
+	private Long pharmacyId;
+
+	/**
+	 * 邮费区域ID
+	 */
+	private Integer areaId;
+
+	/**
+	 * 地区邮费（分）
+	 */
+	private Integer freightPrice;
+
+	
+	/**
+	 * 创建时间
+	 */
+	private Date createdAt;
+
+	/**
+	 * 创建人
+	 */
+	private String createdBy;
+
+	/**
+	 * 修改时间
+	 */
+	private Date changedAt;
+
+	/**
+	 * 修改人
+	 */
+	private String changedBy;
+
+	/**
+	 * 乐观锁
+	 */
+	private Integer version;
+
+	/**
+	 * 是否删除 0未删除，1删除
+	 */
+	private Integer delFlag;
+}
---
+++ health-mall-product-webapp/src/main/java/com/drstrong/health/product/dao/store/OldFreightPostageMapper.java
@@ -0,0 +1,13 @@
+package com.drstrong.health.product.dao.store;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.drstrong.health.product.model.entity.store.OldAreaFreight;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author xieYueFeng
+ * @Date 2022/07/26/9:26
+ */
+@Mapper
+public interface OldFreightPostageMapper extends BaseMapper<OldAreaFreight> {
+}
