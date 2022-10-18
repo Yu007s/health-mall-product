e2ac66c3028d7eb2adc9c6a80ab8a713ee49dccf
Yu007s <75481700+Yu007s@users.noreply.github.com>
2022-10-18 17:56:32 +0800

    feat： 1、补充缺失的@data注解

--- health-mall-product-remote-api/src/main/java/com/drstrong/health/product/remote/model/ChineseMedicineDTO.java
+++ health-mall-product-remote-api/src/main/java/com/drstrong/health/product/remote/model/ChineseMedicineDTO.java
@@ -2,9 +2,12 @@ package com.drstrong.health.product.remote.model;
 
 
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Data
 public class ChineseMedicineDTO implements Serializable {
 
     private static final long serialVersionUID = -5997425803076497105L;
