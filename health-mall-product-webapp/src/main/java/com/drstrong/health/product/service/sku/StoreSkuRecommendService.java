50f9a6b29c019b1882a31760b35ca786be7025fc
liuqiuyi <liuqiuyi@cnstrong.cn>
2023-07-07 15:58:36 +0800

    fix(供应链三期-活动管理&处方推荐): 提交部分代码保存

---
+++ health-mall-product-webapp/src/main/java/com/drstrong/health/product/service/sku/StoreSkuRecommendService.java
@@ -0,0 +1,12 @@
+package com.drstrong.health.product.service.sku;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.drstrong.health.product.model.entity.sku.StoreSkuRecommendEntity;
+
+/**
+ * @author liuqiuyi
+ * @date 2023/6/8 14:47
+ */
+public interface StoreSkuRecommendService extends IService<StoreSkuRecommendEntity> {
+
+}
